009121790ed5f3e210276fe58b29a3ad0ca91021
zhengzhendu <zdzhengzhendu@163.com>
2020-06-25 11:58:53 +0800

    希尔排序

---
+++ src/main/java/com/haha/sort/Shell.java
@@ -0,0 +1,59 @@
+package com.haha.sort;
+
+/**
+ * 希尔排序
+ * 选定一个增长量，按照增长量对数据分组
+ * 对分好组的每一组数据进行插入排序
+ * 减少增长量(最小减为1)，重复上一步骤
+ * 注意：每组排序不是先后执行的(A组执行完排序，再执行B组排序)，而是每组轮流执行一轮(A排序一轮，B排序一轮，A排序一轮，B排序一轮，直到都排序完)
+ * <p>
+ * 增长量确定规则，目前没有说最好的增长量要是多少好，个说个话
+ * int h=1;
+ * for(h<arr.length){
+ * h=2*h+1;
+ * }
+ * 增长量减小规则
+ * h = h/2
+ */
+public class Shell {
+
+    // 对数组a中的元素进行排序
+    public static void sort(Comparable[] a) {
+        //确定增长量
+        int h = 1;
+        while (h < a.length / 2) {
+            h = 2 * h + 1;
+        }
+        // 当增长量小于1时，排序结束
+        while (h >= 1) {
+            //找到待插入的元素
+            for (int i = h; i < a.length; i++) {
+                // 将待插入的元素插入到有序数列中
+                for (int j = i; j >= h; j -= h) {
+                    // 待插入的元素是a[j],比较a[j]和a[j-h]
+                    if (greater(a[j - h], a[j])) {
+                        //交换元素
+                        exch(a, j - h, j);
+                    } else {
+                        //如果不大于，则待插入元素已找到合适位置
+                        break;
+                    }
+                }
+            }
+            h = h / 2;
+        }
+    }
+
+    // 比较c1是否大于c2;
+    private static boolean greater(Comparable c1, Comparable c2) {
+        int result = c1.compareTo(c2);
+        return result > 0;
+    }
+
+    // 交换下标i和j的元素
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
---
+++ src/main/java/com/haha/test/ShellTest.java
@@ -0,0 +1,79 @@
+package com.haha.test;
+
+
+import com.haha.sort.Insert;
+import com.haha.sort.Shell;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShellTest {
+
+    public static void main(String[] args) throws IOException {
+
+//        createFile();
+        List<Integer> list = new ArrayList<Integer>();
+
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(ShellTest.class.getClassLoader().getResourceAsStream("reverse_arr.txt")));
+
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            int i = Integer.parseInt(line);
+            list.add(i);
+        }
+        bufferedReader.close();
+        Integer[] arr = new Integer[list.size()];
+        list.toArray(arr);
+
+        //最坏情况测试
+        //测试插入排序,10万条数据，使用时间 23146 ms
+//        testInsertSort(arr);
+        //测试希尔排序，10万条数据，使用时间 25ms
+        testShellSort(arr);
+    }
+
+
+    /**
+     * 测试希尔排序
+     *
+     * @param arr
+     */
+    public static void testShellSort(Integer[] arr) {
+        long start = System.currentTimeMillis();
+        Shell.sort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println("shell sort used time =" + (end - start));
+    }
+
+    /**
+     * 测试插入排序
+     *
+     * @param arr
+     */
+    public static void testInsertSort(Integer[] arr) {
+        long start = System.currentTimeMillis();
+        Insert.sort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println("insert sort used time =" + (end - start));
+    }
+
+    /**
+     * 生成测试txt
+     *
+     * @throws IOException
+     */
+    public static void createFile() throws IOException {
+
+        File file = new File("./reverse_arr.txt");
+        file.createNewFile();
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 100000; i > 0; i--) {
+            stringBuffer.append(i + "\n");
+        }
+        fileOutputStream.write(stringBuffer.toString().getBytes());
+        fileOutputStream.close();
+    }
+}
